d151fa82e9eb0decf334e8823f9605f11575edec
Heique <148976981+HeiqueJS@users.noreply.github.com>
2023-10-26 10:15:08 -0300

    for4.js

--- Para/for4.js
+++ Para/for4.js
@@ -1,11 +1,10 @@
 const leitor=require("readline-sync")
 
-let num= leitor.questionInt("Insira o número que quer fatorar: ");
+let num = leitor.questionInt("insira um numero para descobrir seu fatorial: ")
+let fator = 1;
 
-let fator=1;
-
-for(let i = num; i >=1; i--){
-    
-    console.log(i );
-
+for(let i = num; i >= 1; i--){
+    fator *= i;
 }
+
+console.log('O fatorial de '+num +" é " +(fator))
