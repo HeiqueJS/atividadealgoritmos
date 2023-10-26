2b8b5974b960c43c08104910162315d1f34d76b8
Heique <148976981+HeiqueJS@users.noreply.github.com>
2023-10-26 10:12:43 -0300

    for2.js

--- Para/for2.js
+++ Para/for2.js
@@ -1,4 +1,8 @@
+const leitor=require("readline-sync")
+
+let numero=leitor.questionInt("Insira um numero que queria cria uma tabuada ate 10: ")
+
 for (let i = 1; i < 11; i++){
 	
-	console.log((i)+" igual a "+(i*5));
+	console.log((i)+`${i} veses ${numero} resulta em ${i*numero} `);
 }
